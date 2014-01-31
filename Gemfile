15f99ef9edbe259e095d64242a2a6879d2ccdca5
Phan Anh Tuan <phananhtuan1011@gmail.com>
2014-01-31 16:43:15 +0700

    Set up the Devise gem

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', '~> 3.0.3.0'
+gem "devise", "~> 3.2.2"
 
 group :development, :test do 
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Require for Heroku
+  #Note to set this to your actual host account
+  config.action_mailer.default_url_options = { :host => 'soloism.herokuapp.com' }
+
 end
