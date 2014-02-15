afe67a09bc2e208decaa9b073c49c5b517f69c88
Phan Anh Tuan <phananhtuan1011@gmail.com>
2014-02-15 10:23:46 +0700

    Try to launch Heroku again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,7 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, 
-		:style => { :medium => "300x300>", :thumb => "100x100>" },
+		:style => { :medium => "300x300>", :thumb => "100x100>" }
 		
 	validates :image, presence: true
   validates :description, presence: true
