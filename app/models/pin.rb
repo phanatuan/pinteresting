fe7b1d55d14d5153197643752488138a68638c72
Phan Anh Tuan <phananhtuan1011@gmail.com>
2014-02-15 10:20:09 +0700

    Try to configure AWS Upload Paperclip again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :style => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, 
+		:style => { :medium => "300x300>", :thumb => "100x100>" },
+		
 	validates :image, presence: true
   validates :description, presence: true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,12 @@ Pinteresting::Application.configure do
 
   # config/environments/production.rb
   # Set paperclip to upload to Amazon S3
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+ config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+}
 end
